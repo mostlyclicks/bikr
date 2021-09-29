bf57aba0896848bc3ab7116a237d3ff2fca474ac
mostlyclicks <carlos@mostlyclicks.com>
2021-09-29 17:01:22 -0600

    add mech/electric options

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { FrontDriveOptions, DriveBrandOptions } from './components/DATA/Data'
+import { FrontDriveOptions, DriveBrandOptions, MechElectronicOptions, MechOptions, ElectronicOptions } from './components/DATA/Data'
 import SelectInput from './components/UI/SelectInput';
 
 
@@ -8,6 +8,7 @@ function App() {
 
   const frontDriveOptions = FrontDriveOptions
   const driveBrandOptions = DriveBrandOptions
+  const mechElectronicOptions = MechElectronicOptions
 
   console.log(frontDriveOptions)
 
@@ -19,6 +20,12 @@ function App() {
       
           <SelectInput label="Front Drive Options" selectInitialValue="Please select 1x or 2x" selectOptions={frontDriveOptions} />
 
+          <SelectInput label="Drivetrain Brand" selectInitialValue="Please select drivetrain brand" selectOptions={driveBrandOptions} />
+
+          <SelectInput label="Mechanical or Electric Shifting" selectInitialValue="Please select" selectOptions={mechElectronicOptions} />
+
+          <SelectInput label="Mechanical Drivetrain Options" selectInitialValue="Please drivetrain brand" selectOptions={driveBrandOptions} />
+          
           <SelectInput label="Drivetrain Brand" selectInitialValue="Please drivetrain brand" selectOptions={driveBrandOptions} />
       
 
--- src/components/DATA/Data.js
+++ src/components/DATA/Data.js
@@ -8,8 +8,31 @@ const DriveBrandOptions = [
   "SRAM"
 ]
 
+const MechElectronicOptions = [
+  "Mechanical",
+  "Electronic"
+]
+
+const MechOptions = [
+  "SRAM Force",
+  "SRAM Rival",
+  "SRAM GX",
+  "SRAM NX",
+  "Shimano Ultegra",
+  "Shimano Dura-Ace"
+]
+
+const ElectronicOptions = [
+  "SRAM Force AXS",
+  "SRAM Red AXS",
+  "Shimano Ultegra Di2",
+  "Shimano Dura-Ace Di2"
+]
 
 
 
 
-export { FrontDriveOptions, DriveBrandOptions }
+
+
+
+export { FrontDriveOptions, DriveBrandOptions, MechElectronicOptions, MechOptions, ElectronicOptions }
