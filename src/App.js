8fb2f0ed61b998659a313b9c8b84aa16c2d9a833
mostlyclicks <carlos@mostlyclicks.com>
2021-09-29 15:26:58 -0600

    add static select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bike Builder</h1>
+      <form>
+        <select>
+          <option value=''>Please select</option>
+          <option value="1x">1x</option>
+          <option value="2x">2x</option>
+        </select>
+      </form>
+
     </div>
   );
 }
