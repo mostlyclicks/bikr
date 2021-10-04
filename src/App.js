823407aefbb704eb152ad5aa81661f0ccb96d573
mostlyclicks <carlos@mostlyclicks.com>
2021-10-03 19:03:08 -0600

    get test select working

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 
   const [frontDrive, setFrontDrive] = useState('')
 
+
   const frontDriveChangeHandler = (event) => {
     const selection = event.target.value
     setFrontDrive(selection)
@@ -27,13 +28,28 @@ function App() {
     console.log(frontDrive)
   }
 
+
+  const [testState, setTestState] = useState('')
+  const testHandler = (e) => {
+    console.log(e.target.value)
+    setTestState(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Bildr</h1>
       
-      <form onSubmit={handleSubmit}>      
+      <form onSubmit={handleSubmit}>
+      
+        <select onChange={testHandler} value={testState}>
+          <option>Please select</option>
+          <option>Test 1</option>
+          <option>Test 2</option>
+          <option>Test 3</option>
+        </select>
+
        
-        <SelectInput onChange={frontDriveChangeHandler} value={frontDrive} label="Front Drive Options" selectInitialValue="Please select 1x or 2x" selectOptions={frontDriveOptions} />
+        <SelectInput onChange={frontDriveChangeHandler} selected={frontDrive} label="Front Drive Options" selectInitialValue="Please select 1x or 2x" selectOptions={frontDriveOptions} />
 
         <SelectInput label="Drivetrain Brand" selectInitialValue="Please select drivetrain brand" selectOptions={driveBrandOptions} />
 
@@ -49,6 +65,7 @@ function App() {
       <div>
         <h2>Selections</h2>
         <p>{frontDrive}</p>
+        <p>{testState}</p>
       </div>
 
 
